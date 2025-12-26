f25b8c04d137ccdaa26234606dd2fbba4de17a00
TheQjc <3063751837@qq.com>
2025-12-26 14:09:34 +0800

    修复鉴权

--- src/main/java/com/usst/adfluxbackend/controller/AdvertiserController.java
+++ src/main/java/com/usst/adfluxbackend/controller/AdvertiserController.java
@@ -24,7 +24,7 @@ import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/advertisers")
-@RequireRole("ADVERTISER")
+@RequireRole("advertiser")
 public class AdvertiserController {
 
     @Resource
